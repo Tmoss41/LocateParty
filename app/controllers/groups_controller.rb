6c8a39b5cd9a08be2fec8dbc5cab15313f44078a
Timothy Moss <tim@Timothys-MacBook-Pro.local>
2021-05-14 15:16:56 +1000

    Added in group show page and new group form to geoup controller and added in params permit method for groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,4 +5,15 @@ class GroupsController < ApplicationController
     end
     def show
     end
+    def new
+        @group = Group.new
+    end
+    def create
+        @group = current_user.groups.create(group_params)
+        redirect_to groups_path
+    end
+    private
+    def group_params
+        params.require(:group).permit(:name)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions' }
   get 'user/:id', to: 'profiles#index', as: 'user'
   root to: 'home#index'
-  resources :characters
+  resources :characters, :groups
   get 'user/:id/characters/new', to: 'characters#new', as: 'user_new_character'
-  get 'groups', to: 'groups#index', as: 'groups'
+  get 'groups', to: 'groups#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
