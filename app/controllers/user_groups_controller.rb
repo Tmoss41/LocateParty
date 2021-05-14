6a3d89120613cb0d6c39b1363f5369635eba6a3b
Timothy Moss <tim@Timothys-MacBook-Pro.local>
2021-05-14 19:37:25 +1000

    Addition of uniqueness validations at model level to ensure that users can not repeatedly join a group

--- app/controllers/user_groups_controller.rb
+++ app/controllers/user_groups_controller.rb
@@ -1,8 +1,8 @@
 class UserGroupsController < ApplicationController
   before_action :set_group_id, only: [:join]
   def join
-    @query = UserGroup.create(user_id: current_user.id, group_id: @group)
-    redirect_to current_user
+      @query = UserGroup.create(user_id: current_user.id, group_id: @group)
+      redirect_to current_user
   end
   private 
   def set_group_id
--- app/models/user_group.rb
+++ app/models/user_group.rb
@@ -1,4 +1,5 @@
 class UserGroup < ApplicationRecord
   belongs_to :user
   belongs_to :group
+  validates :user_id , uniqueness: { scope: [:user_id, :group_id] }
 end
